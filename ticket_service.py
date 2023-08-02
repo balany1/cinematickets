b002cf877cfb52e94399075f2dedf84442976ded
balany1 <andrewmcnamara@live.co.uk>
2023-08-02 17:14:31 +0100

    unit tests updated

--- ticket_service.py
+++ ticket_service.py
@@ -15,18 +15,75 @@ class TicketService:
 
     def __init__(self) -> None:
         self.seat_reservation_service = SeatReservationService()
-        self.ticket_type_request = TicketTypeRequest()
         self.ticket_payment_service = TicketPaymentService()
         
-        
+    def __check_adult_ticket(self,number_of_tickets):
+        if number_of_tickets == 0:
+            raise InvalidPurchaseException
 
-    def purchase_tickets(account_id=None, ticket_type_requests=[]):
+    def __check_max_tickets(self,number_of_tickets):
+        if number_of_tickets >= 20:
+            raise InvalidPurchaseException
 
+    def __check_unaccompanied_infant(self,number_of_adults,number_of_infants):
+        if number_of_infants > number_of_adults:
+            raise InvalidPurchaseException
+
+    def __check_account_id_pos(self, account_id):
+        if account_id <=0:
+            raise InvalidPurchaseException
+    
+    def __check_account_id_int(self, account_id):
+        if type(account_id) != int:
+            raise InvalidPurchaseException
+
+    def purchase_tickets(self, account_id=None, ticket_type_requests=[]):
+
+        #loop through my tickets and get data
+      no_of_adults = 0
+      no_of_children = 0
+      no_of_infants = 0
+
+      for ticket_request in ticket_type_requests:
+          if ticket_request.get_ticket_type() == "ADULT":
+              no_of_adults = no_of_adults + ticket_request.get_tickets_number()
+          elif ticket_request.get_ticket_type() == "CHILD":
+              no_of_children = no_of_children + ticket_request.get_tickets_number()
+          elif ticket_request.get_ticket_type() == "INFANT":
+              no_of_infants = no_of_infants + ticket_request.get_tickets_number()
         
+      #use this information to determine number of seats required
+
+      no_of_seats = no_of_adults + no_of_children
+
+      #use this information to determine number of tickets required
+
+      no_of_tickets = no_of_adults + no_of_children + no_of_infants
+
+      #perform checks on constraints
+
+      self.__check_adult_ticket(no_of_adults)
+      self.__check_max_tickets(no_of_tickets)
+      self.__check_unaccompanied_infant(no_of_adults, no_of_infants)
+      self.__check_account_id_int(account_id)
+      self.__check_account_id_pos(account_id)
+      
+      #use inputs to determine cost of tickets required
+
+      cost = 20*no_of_adults + 10*no_of_children
+
+      # print(no_of_adults)
+      # print(no_of_children)
+      # print(no_of_infants)
+      # print(no_of_seats)
+      # print(no_of_tickets)
+      # print(cost)
+       
+              
         #raise InvalidPurchaseException
     
-        #self.seat_reservation_service.reserve_seat(account_id, no_of_seats)
-        #self.ticket_payment_service.make_payment(account_id, cost)
+      self.seat_reservation_service.reserve_seat(account_id, no_of_seats)
+      self.ticket_payment_service.make_payment(account_id, cost)
             
         
 
--- unit_test.py
+++ unit_test.py
@@ -22,23 +22,40 @@ class Test_ticket_service(unittest.TestCase):
 
     def test_tickets_requested(self):
         #tests that there has been a ticket requested
-        number_of_tickets = self.ticket_service.purchase_tickets(account_id=1, ticket_type_requests=self.my_tickets)
+        number_of_tickets = 0
+        for ticket_request in self.my_tickets:
+            number_of_tickets = number_of_tickets + ticket_request.get_tickets_number()
+
         self.assertGreater(number_of_tickets, 0)
-        pass
+        
 
     def test_max_tickets(self):
         #tests that the number of tickets does not exceed 20
-        number_of_tickets = self.ticket_service.purchase_tickets(account_id=1, ticket_type_requests=self.my_tickets)
+        number_of_tickets = 0
+        for ticket_request in self.my_tickets:
+            number_of_tickets = number_of_tickets + ticket_request.get_tickets_number()
         self.assertLessEqual(number_of_tickets, 20)
         pass
 
     def test_no_of_adults(self):
-        #tests that there is at least one adult
-        pass
+        no_of_adults = 0
+        for ticket_request in self.my_tickets:
+            if ticket_request.get_ticket_type() == "ADULT":
+              no_of_adults = no_of_adults + ticket_request.get_tickets_number()
+        
+        self.assertGreater(no_of_adults,0)
 
     def test_unnaccompanied_minors(self):
-        #tests that there aren't children travelling alone
-        pass
+        #tests that there aren't more infants than adults
+        no_of_adults = 0
+        no_of_infants = 0
+        for ticket_request in self.my_tickets:
+            if ticket_request.get_ticket_type() == "ADULT":
+              no_of_adults = no_of_adults + ticket_request.get_tickets_number()
+            if ticket_request.get_ticket_type() == "INFANT":
+              no_of_infants = no_of_infants + ticket_request.get_tickets_number()
+        
+        self.assertGreater(no_of_adults,no_of_infants)
 
 if __name__ == "__main__":
     unittest.main()
