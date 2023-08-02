89b156c0f03dbd787f79904c6a3e7fb8ff2e2662
balany1 <andrewmcnamara@live.co.uk>
2023-08-02 16:50:51 +0100

    more unit tests and refactor

--- main.py
+++ main.py
@@ -8,41 +8,7 @@ from paymentgateway.ticket_payment_service import TicketPaymentService
 
 if __name__ == '__main__':
 
-    my_tickets = [TicketTypeRequest("ADULT", 2),TicketTypeRequest("CHILD"),3]
-
-    ticket_service = TicketService(account_id=1, t)
-    ticket_type = TicketTypeRequest()
-    ticket_type_requests = []
-
-    #set series of inputs to determine customer requirements
-    account_id = int(input("Please enter your account id:"))
-
-    #check at least one adult is travelling
-    valid = False
-    while not valid:
-        no_of_adults = int(input("Please enter number of adults travelling:")).get_tickets_number()
-        if no_of_adults >= 1:
-            valid = True
-        else:
-            print("There must be at least one adult travelling")
-            continue
-    
-    
-    #check number of dependents         
-    no_of_children =int(input("Please enter number of children travelling:"))
-    no_of_infants = int(input("Please enter number of infants travelling:"))
-
-    #use this information to determine number of seats required
-
-    no_of_seats = no_of_adults + no_of_children
-
-    #use this information to determine number of tickets required
-
-    no_of_tickets = no_of_adults + no_of_children + no_of_infants
-
-    #use inputs to determine cost of tickets required
-
-    cost = 20*no_of_adults + 10*no_of_children
-
-
-    ticket_service.purchase_tickets(account_id,ticket_type_requests)
+    #instantiate TicketService and list of tickets
+    ticket_service = TicketService()
+    my_tickets = [TicketTypeRequest("ADULT", 2),TicketTypeRequest("CHILD",3),TicketTypeRequest("ADULT", 2)]
+    ticket_service.purchase_tickets(account_id = 1,ticket_type_requests=my_tickets)
--- unit_test.py
+++ unit_test.py
@@ -1,32 +1,35 @@
 import unittest
+import builtins
 from ticket_service import TicketService
 from ticket_type_request import TicketTypeRequest
+from purchase_exceptions import InvalidPurchaseException
 
 class Test_ticket_service(unittest.TestCase):
 
     def setUp(self) -> None:
-        my_tickets = [TicketTypeRequest("ADULT", 2),TicketTypeRequest("CHILD",3)]
-        self.ticket_service = TicketService.purchase_tickets(account_id=1,ticket_type_requests=my_tickets)
-        return super().setUp()
-    
-    def tearDown(self) -> None:
-        return super().tearDown()
-    
+        self.ticket_service = TicketService()
+        self.my_tickets = [TicketTypeRequest("ADULT", 2),TicketTypeRequest("CHILD",3)]
+        
     def test_no_account_id(self):
         #tests that there is an account_id
-        account_id = self.ticket_service.purchase_tickets(account_id=1)
-        self.assertIsInstance(account_id, int)
+        with self.assertRaises(InvalidPurchaseException):
+            self.ticket_service.purchase_tickets(account_id=None)
         
     def test_negzero_account_id(self):
         account_id = self.ticket_service.purchase_tickets(account_id=1)
+        print(account_id)
         self.assertGreater(account_id, 0)
 
     def test_tickets_requested(self):
         #tests that there has been a ticket requested
+        number_of_tickets = self.ticket_service.purchase_tickets(account_id=1, ticket_type_requests=self.my_tickets)
+        self.assertGreater(number_of_tickets, 0)
         pass
 
     def test_max_tickets(self):
         #tests that the number of tickets does not exceed 20
+        number_of_tickets = self.ticket_service.purchase_tickets(account_id=1, ticket_type_requests=self.my_tickets)
+        self.assertLessEqual(number_of_tickets, 20)
         pass
 
     def test_no_of_adults(self):
